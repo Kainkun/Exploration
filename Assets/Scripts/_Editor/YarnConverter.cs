a652d0735098a83b828adaaf51f85a6caed2074f
Kain Shepard <kainkun100@gmail.com>
2022-09-07 16:33:06 -0700

    organization

--- Assets/Scripts/_Editor/YarnConverter.cs
+++ Assets/Scripts/_Editor/YarnConverter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,31 +9,39 @@ using System.Text.RegularExpressions;
 
 public class YarnConverter : EditorWindow
 {
-    const string noColor = @"(?!\<\/color\>)";
-    const string word = "your";
-    const string replace = @"<color=\#ffffff>$&</color>";
-
+    private readonly (string[], string)[] pairs = new (string[], string)[]
+    {
+        (new string[] { "grey" }, "9e9e9e"), //Computer Variable
+        (new string[] { "error" }, "ff0000"), //Computer Error
+        (new string[] { "vulcan", "Vulcan", "Vulcan Terminal" }, "ff0000"), //Vulcan
+        (new string[] { "multiTool", "Standard Lab Access Keycard" }, "fcba03") //Items
+    };
 
     [MenuItem("Yarn Tools/Convert")]
     private static void ConvertFiles()
     {
-        string path = Application.dataPath;
-        path += "/Yarn";
-        string[] files = Directory.GetFiles(path, "*.yarn", SearchOption.AllDirectories);
+        string[] files = Directory.GetFiles(Application.dataPath + "/Yarn/Original", "*.yarn", SearchOption.AllDirectories);
         foreach (string file in files)
             ConvertFile(file);
     }
 
-    public static void ConvertFile(string filePath)
+    public static void ConvertFile(string originalFilePath)
     {
-        string[] lines = File.ReadAllLines(filePath);
+        string[] lines = File.ReadAllLines(originalFilePath);
 
-         // for (int i = 0; i < lines.Length; i++)
-         // {
-         //     Regex regex = new Regex(word + noColor);
-         //     lines[i] = regex.Replace(lines[i], replace);
-         // }
+        for (int i = 0; i < lines.Length; i++)
+        {
+            foreach (var pair in pairs)
+            {
+                foreach (string s in pair.Item1)
+                {
+                    Regex regex = new Regex(s + @"(?!\<\/color\>)");
+                    lines[i] = regex.Replace(lines[i], @"<color=\#" + pair.Item2 + @">$&</color>");
+                }
+            }
+        }
 
-        File.WriteAllLines(filePath, lines);
+        string processedFilePath = originalFilePath.Replace(Application.dataPath + "/Yarn/Original", Application.dataPath + "/Yarn/Processed");
+        File.WriteAllLines(originalFilePath, lines);
     }
 }
